558e8fdc94bfa6ad2496a46ad65bdb76d55e26bf
LeoOlivaresD <cspraword@gmail.com>
2023-05-03 16:39:40 -0400

    Initial commit

---
+++ src/test/java/cl/javadevs/springsecurityjwt/SpringSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.javadevs.springsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
