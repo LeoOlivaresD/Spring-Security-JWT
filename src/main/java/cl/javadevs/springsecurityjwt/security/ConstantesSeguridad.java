aba13569daf8c5b6da7948050bbf29ac5e3201b8
LeoOlivaresD <cspraword@gmail.com>
2023-05-03 18:57:20 -0400

    Creación de constantes de seguridad

---
+++ src/main/java/cl/javadevs/springsecurityjwt/security/ConstantesSeguridad.java
@@ -0,0 +1,6 @@
+package cl.javadevs.springsecurityjwt.security;
+
+public class ConstantesSeguridad {
+    public static final long JWT_EXPIRATION_TOKEN = 300000;
+    public static final String JWT_FIRMA = "firma";
+}
