cee68213a081d396255a6a6cbbfe0717e0f69a49
LeoOlivaresD <cspraword@gmail.com>
2023-05-03 22:27:19 -0400

    Creación de RestControllerAuth

---
+++ src/main/java/cl/javadevs/springsecurityjwt/controllers/RestControllerAuth.java
@@ -0,0 +1,85 @@
+package cl.javadevs.springsecurityjwt.controllers;
+
+import cl.javadevs.springsecurityjwt.dtos.DtoAuthRespuesta;
+import cl.javadevs.springsecurityjwt.dtos.DtoLogin;
+import cl.javadevs.springsecurityjwt.dtos.DtoRegistro;
+import cl.javadevs.springsecurityjwt.models.Roles;
+import cl.javadevs.springsecurityjwt.models.Usuarios;
+import cl.javadevs.springsecurityjwt.repositories.IRolesRepository;
+import cl.javadevs.springsecurityjwt.repositories.IUsuariosRepository;
+import cl.javadevs.springsecurityjwt.security.JwtGenerador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+
+@RestController
+@RequestMapping("/api/auth/")
+public class RestControllerAuth {
+    private AuthenticationManager authenticationManager;
+    private PasswordEncoder passwordEncoder;
+    private IRolesRepository rolesRepository;
+    private IUsuariosRepository usuariosRepository;
+    private JwtGenerador jwtGenerador;
+
+    @Autowired
+
+    public RestControllerAuth(AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder, IRolesRepository rolesRepository, IUsuariosRepository usuariosRepository, JwtGenerador jwtGenerador) {
+        this.authenticationManager = authenticationManager;
+        this.passwordEncoder = passwordEncoder;
+        this.rolesRepository = rolesRepository;
+        this.usuariosRepository = usuariosRepository;
+        this.jwtGenerador = jwtGenerador;
+    }
+    //Método para poder registrar usuarios con role "user"
+    @PostMapping("register")
+    public ResponseEntity<String> registrar(@RequestBody DtoRegistro dtoRegistro) {
+        if (usuariosRepository.existsByUsername(dtoRegistro.getUsername())) {
+            return new ResponseEntity<>("el usuario ya existe, intenta con otro", HttpStatus.BAD_REQUEST);
+        }
+        Usuarios usuarios = new Usuarios();
+        usuarios.setUsername(dtoRegistro.getUsername());
+        usuarios.setPassword(passwordEncoder.encode(dtoRegistro.getPassword()));
+        Roles roles = rolesRepository.findByName("USER").get();
+        usuarios.setRoles(Collections.singletonList(roles));
+        usuariosRepository.save(usuarios);
+        return new ResponseEntity<>("Registro de usuario existoso", HttpStatus.OK);
+    }
+
+    //Método para poder guardar usuarios de tipo ADMIN
+    @PostMapping("registerAdm")
+    public ResponseEntity<String> registrarAdmin(@RequestBody DtoRegistro dtoRegistro) {
+        if (usuariosRepository.existsByUsername(dtoRegistro.getUsername())) {
+            return new ResponseEntity<>("el usuario ya existe, intenta con otro", HttpStatus.BAD_REQUEST);
+        }
+        Usuarios usuarios = new Usuarios();
+        usuarios.setUsername(dtoRegistro.getUsername());
+        usuarios.setPassword(passwordEncoder.encode(dtoRegistro.getPassword()));
+        Roles roles = rolesRepository.findByName("ADMIN").get();
+        usuarios.setRoles(Collections.singletonList(roles));
+        usuariosRepository.save(usuarios);
+        return new ResponseEntity<>("Registro de usuario existoso", HttpStatus.OK);
+    }
+
+    //Método para poder logear un usuario y obtener un token
+    @PostMapping("login")
+    public ResponseEntity<DtoAuthRespuesta> login(@RequestBody DtoLogin dtoLogin) {
+        Authentication authentication = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                dtoLogin.getUsername(), dtoLogin.getPassword()));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtGenerador.generarToken(authentication);
+        return new ResponseEntity<>(new DtoAuthRespuesta(token), HttpStatus.OK);
+    }
+
+
+}
