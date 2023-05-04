97f5526d16b99ff0f051b1f4791f84dab7f97d35
LeoOlivaresD <cspraword@gmail.com>
2023-05-03 21:35:41 -0400

    Se cambió modificador de acceso de método obtenerJwtDeLaSolicitud de público a privado

--- src/main/java/cl/javadevs/springsecurityjwt/security/JwtAuthenticationFilter.java
+++ src/main/java/cl/javadevs/springsecurityjwt/security/JwtAuthenticationFilter.java
@@ -16,7 +16,8 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 import java.util.List;
 
-//La función de esta clase será validar la información del token y si esto es exitoso, establecerá la autenticación de un usuario en la solicitud o en el contexto de seguridad de nuestra aplicacion
+/*La función de esta clase será validar la información del token y si esto es exitoso,
+establecerá la autenticación de un usuario en la solicitud o en el contexto de seguridad de nuestra aplicación*/
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     @Autowired
@@ -26,7 +27,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     /*Con el siguiente método extraeremos  el token JWT de la cabecera de nuestra petición Http("Authorization")
      * luego lo validaremos y finalmente se retornará*/
-    public String obtenerTokenDeSolicitud(HttpServletRequest request) {
+    private String obtenerTokenDeSolicitud(HttpServletRequest request) {
         String bearerToken = request.getHeader("Authorization");
         if (StringUtils.hasText(bearerToken) && bearerToken.startsWith("Bearer ")) {
             //Aca si se encuentra el token JWT, se devuelve una subcadena de "bearerToken" que comienza después de los primeros 7 caracteres hasta el final de la cadena
