24afc095220193ad87b152f1cdaeaf8f451949d2
LeoOlivaresD <cspraword@gmail.com>
2023-05-04 15:08:06 -0400

    Documentación

--- src/main/java/cl/javadevs/springsecurityjwt/security/ConstantesSeguridad.java
+++ src/main/java/cl/javadevs/springsecurityjwt/security/ConstantesSeguridad.java
@@ -1,6 +1,6 @@
 package cl.javadevs.springsecurityjwt.security;
 
 public class ConstantesSeguridad {
-    public static final long JWT_EXPIRATION_TOKEN = 300000;
+    public static final long JWT_EXPIRATION_TOKEN = 300000; //equivaler a 5 min, donde 60000 = a 1 min
     public static final String JWT_FIRMA = "firma";
 }
